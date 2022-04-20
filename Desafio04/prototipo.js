755d17a71ba7f34131e3f9f02a70a0a8cb16a131
unknown <wendel.vieira.dev@gmail.com>
2022-04-19 22:31:30 -0300

    adicionado

--- Desafio04/prototipo.js
+++ Desafio04/prototipo.js
@@ -1,15 +1,19 @@
 $(document).ready(function () {
     $("#buttonbars").click(function () {
+        $("#lista").toggle();
+
+    });
+});
+
+/*esse é a mesma coisa que o visible
         if ($("#form").is(":visible")) {
             $("#form").hide();
         } else {
             $("#form").show();
-        }
-    })
-});
-
+        }*/
+        
 $(window).resize(function () {
     if ($(document).innerWidth() > 600) {
-        $("#form").hide();
+        $("#lista").hide();
     }
 });
