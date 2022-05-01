351641d8fae30b42181cbb17cddcf599f7aca6cf
unknown <wendel.vieira.dev@gmail.com>
2022-05-01 13:39:53 -0300

    atualizado

---
+++ angular/Desafio_5.1/src/app/components/interpolacao/interpolacao.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-interpolacao',
+  templateUrl: './interpolacao.component.html',
+  styleUrls: ['./interpolacao.component.css']
+})
+export class InterpolacaoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
